4988b9df03be7ea5a8c26ac017d471c3c2b6ac49
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-20 15:38:45 +0200

    no message

--- Aquarium/Aquarium/Program.cs
+++ Aquarium/Aquarium/Program.cs
@@ -11,6 +11,30 @@ namespace Aquarium
         static void Main(string[] args)
         {
 
+            string[0, 0] aquarium = new string[10, 10];
+
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; 1 < 10; i++)
+                {
+                    aquarium[i, j] = "a";
+                }
+            }
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; 1 < 10; i++)
+                {
+                    Console.Write(aquarium[i, j]);
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadLine();
+
+
+
+
+
         }
     }
 }
